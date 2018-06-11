a82e7cd774e66e615b469ee68e999fd326925533
Xavi <mr.xavifuentes@gmail.com>
2018-06-11 11:24:00 +0100

    configure yarn workspaces

--- packages/client/src/root/components/Root.js
+++ packages/client/src/root/components/Root.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+
+import { Button } from 'ui';
 
 const App = () => (
   <div>
     <header>
       <h1>Welcome to a monorepo</h1>
+
+      <Button
+        onClick={() => {
+          console.log('clicked!');
+        }}
+      >
+        Click me!
+      </Button>
     </header>
   </div>
 );
